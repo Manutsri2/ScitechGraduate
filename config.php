2d14101671051faec5925876425ffe37c7037670
Manutsri2 <33697788+Manutsri2@users.noreply.github.com>
2018-05-05 16:37:46 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+	$host = "localhost";
+	$user = "root";
+	$pw = "";
+	$database = "project01";
+
+	mysql_connect($host, $user, $pw) or die ("Can't connect mysql");
+	mysql_select_db($database) or die ("Can't select database");
+	mysql_query("SET NAMES UTF8");
+
+?>
